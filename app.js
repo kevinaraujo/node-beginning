d4340f094bc2ee07b17112a28007b858fd583d2f
Kevin Fonseca De Araújo <kevin.araujo@ideal-986.idealinvest.local>
2020-02-22 15:42:43 -0300

    working with http

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-let a = 2;
-let b = '2';
+var http = require('http');
 
-let c = a + b;
-console.log('Hello world', c);
+function handle (request, response) {
+    
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    response.write('<!DOCTYPE "html">');
+    response.write('<html><body><h1>Hello from http module</h1></body></html>');
+
+    response.end();
+}
+
+var server = http.createServer(handle);
+
+server.listen(9000, function() {
+    console.log('Server is listening at 9000 port.')
+});
